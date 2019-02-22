f50de565994a87b56489af72e843a893f447b310
ocampodar <47321346+ocampodar@users.noreply.github.com>
2019-02-22 12:28:44 -0300

    clase 6

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,9 +2,14 @@ function Mostrar()
 {
 
 var sexo = prompt("ingrese f ó m .");
+while (!(sexo == "f" || sexo == "m")) {
+     sexo = prompt("Incorrecto. Ingese su sexo nuevamente");
+
+    }
+    
 
 
 
-document.getElementById('Sexo').value=sexo;
+document.getElementById('Sexo').value = sexo;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,20 @@
-function Mostrar()
-{
+function Mostrar() {
 
-	var contador=0;
-	var acumulador=0;
+    var contador = 0;
+    var acumulador = 0;
+    var num;
 
 
+    while (contador < 5) {
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+        num = parseInt(prompt("Ingrese un numeros"))
+
+        acumulador = acumulador + num
+        contador = contador + 1
+    }
+
+    promedio = acumulador / 5
+    document.getElementById("suma").value = acumulador;
+    document.getElementById("promedio").value = promedio;
 
 }//FIN DE LA FUNCIÓN
