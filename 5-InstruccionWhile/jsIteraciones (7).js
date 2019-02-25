86e60aed45b2ba8025a96d1b6f35065a1796ac9f
ocampodar <47321346+ocampodar@users.noreply.github.com>
2019-02-24 22:34:14 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,12 +1,20 @@
-function Mostrar()
-{
+function Mostrar() {
 
-	var contador=0;
-	var acumulador=0;
-	var respuesta='si';
+	var contador = 0;
+	var acumulador = 0;
+	var respuesta = 'si';
+	var num;
+	do {
+		num = parseInt(prompt("Ingrese un numeros"))
+		respuesta = prompt("Quiere ingresar otro numero?")
+		acumulador = acumulador + num
+		contador = contador + 1
 
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+
+	} while (respuesta == "si");
+
+	document.getElementById('suma').value = acumulador;
+	document.getElementById('promedio').value = acumulador / contador;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -6,7 +6,15 @@ function Mostrar()
 	var negativo=1;
 	
 	var respuesta='si';
+	var num;
+	do {
+		num = parseInt(prompt("Ingrese un numeros"))
+		respuesta = prompt("Quiere ingresar otro numero?")
+		
 
+
+
+	} while (respuesta == "si");
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
